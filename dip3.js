3b9c6c67e93dad49fa686ad7b26390b172812eaa
Mark Pilgrim <mark@diveintomark.org>
2009-03-19 01:42:12 -0400

    generate in-page TOCs on demand

--- dip3.js
+++ dip3.js
@@ -16,6 +16,8 @@ $(document).ready(function() {
   }
 */
 
+  $("#toc").html('<a href="javascript:showTOC()">table of contents</a>');
+
   // "hide", "open in new window", and (optionally) "download" widgets on code & screen blocks
   $("pre > code").each(function(i) {
     var pre = $(this.parentNode);
@@ -80,4 +82,24 @@ function plainTextOnClick(id) {
   win.document.open();
   win.document.write('<pre>' + clone.html());
   win.document.close();
+}
+
+function showTOC() {
+  var toc = '';
+  var old_level = 1;
+  $('h2,h3').each(function(i, h) {
+    level = parseInt(h.tagName.substring(1));
+    if (level < old_level) {
+      toc += '</ol>';
+    } else if (level > old_level) {
+      toc += '<ol>';
+    }
+    toc += '<li><a href=#' + h.id + '>' + h.innerHTML + '</a>';
+    old_level = level;
+  });
+  while (level > 1) {
+    toc += '</ol>';
+    level -= 1;
+  }
+  $("#toc").html(toc);
 }
