8919471dd82b9cf92610f62008dee3d64a003f12
Mark Pilgrim <mark@diveintomark.org>
2009-03-29 18:09:43 -0500

    code for Advanced Iterators chapter

---
+++ examples/alphametics.py
@@ -0,0 +1,57 @@
+import re
+import itertools
+
+def solve(puzzle):
+    '''Find solutions to alphametic equations.
+
+    >>> solve('SEND + MORE == MONEY')
+    9567 + 1085 == 10652
+
+    '''
+    words = re.findall('[A-Z]+', puzzle)
+    unique_characters = set(''.join(words))
+    assert len(unique_characters) <= 10
+    first_letters = set(word[0] for word in words)
+    n = len(first_letters)
+    sorted_characters = ''.join(first_letters) + \
+        ''.join(unique_characters - first_letters)
+    characters = tuple(map(ord, sorted_characters))
+    digits = tuple(map(ord, '0123456789'))
+    zero = digits[0]
+    for guess in itertools.permutations(digits, len(characters)):
+        if zero not in guess[:n]:
+            equation = puzzle.translate(dict(zip(characters, guess)))
+            if eval(equation):
+                return equation
+
+if __name__ == '__main__':
+    import sys
+    for puzzle in sys.argv[1:]:
+        print(puzzle)
+        for solution in solve(puzzle):
+            print(solution)
+
+# Copyright (c) 2009, Raymond Hettinger, All rights reserved.
+# Ported to Python 3 and modified by Mark Pilgrim
+# original: http://code.activestate.com/recipes/576615/
+# 
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
