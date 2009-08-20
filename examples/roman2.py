331dde2c82bd98d1aae0cc23cc5e0706de747635
Mark Pilgrim <mark@diveintomark.org>
2009-08-19 21:49:12 -0400

    fix error message in roman2.py

--- examples/roman2.py
+++ examples/roman2.py
@@ -24,7 +24,7 @@ roman_numeral_map = (('M',  1000),
 def to_roman(n):
     '''convert integer to Roman numeral'''
     if n > 3999:
-        raise OutOfRangeError('number out of range (must be less than 3999)')
+        raise OutOfRangeError('number out of range (must be less than 4000)')
 
     result = ''
     for numeral, integer in roman_numeral_map:
