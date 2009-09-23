0bc40cf248f3d43c5781a0b8ea6b4f199437f76d
Mark Pilgrim <mark@diveintomark.org>
2009-09-23 08:44:24 -0400

    back to more descriptive TypeError

--- examples/customserializer.py
+++ examples/customserializer.py
@@ -9,7 +9,7 @@ def to_json(python_object):
     if isinstance(python_object, bytes):
         return {'__class__': 'bytes',
                 '__value__': list(python_object)}
-    raise TypeError
+    raise TypeError(repr(python_object) + ' is not JSON serializable')
 
 def from_json(json_object):
     if '__class__' in json_object:
